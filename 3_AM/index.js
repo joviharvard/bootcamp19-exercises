0c066c17e19ff8a17e5d01b118a54c7990fb7000
joviharvard <tanj@college.harvard.edu>
2019-01-09 12:50:40 -0500

    sick

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+app.use(express.static("public"))
+
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + '/index.html'))
 });
+
+app.get("/second", (req, res) => {
+  res.sendFile(path.join(__dirname + '/second.html'))
+});
+
+app.get("/third", (req, res) => {
+  res.sendFile(path.join(__dirname + '/third.html'))
+});
+
+app.get("/fourth", (req, res) => {
+  res.sendFile(path.join(__dirname + '/fourth.html'))
+});
+
 
 app.listen(3000);
 console.log("Running at port 3000");
